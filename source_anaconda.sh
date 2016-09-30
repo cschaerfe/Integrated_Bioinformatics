622373f7a8df34e474db31d4994d0515cae60ed3
sebwink <winkler@informatik.uni-tuebingen.de>
2016-09-30 10:15:46 +0200

    fixed source_anaconda.sh LD_LIBRARY_PATH

--- source_anaconda.sh
+++ source_anaconda.sh
@@ -1,2 +1,2 @@
 export PATH=/share/opt/x86_64_sl7/anaconda2/bin:$PATH
-export LD_LIBRARY_PATH=/share/opt/x86_64_sl7/anaconda2/lib:$PATH_LIBRARY_PATH
+export LD_LIBRARY_PATH=/share/opt/x86_64_sl7/anaconda2/lib:$LD_LIBRARY_PATH
